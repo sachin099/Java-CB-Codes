3ad1118a569aca9723c367e99adf4be6bf787c15
Sachin Choudhary <sachin.choudhary58@gmail.com>
2023-03-24 18:03:18 +0530

    Create ReturnNthFibo.java

---
+++ ReturnNthFibo.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class ReturnNthFibo {
+    public static void main(String[] args) {
+        while(true)  {
+        int first = 0;
+        int second = 1;
+        int counter = 2;
+        int nthFibo = 0;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n<2) {
+            nthFibo = 0;
+        }
+        else if(n == 2) {
+            nthFibo = 1;
+        }
+        else {
+            while(counter<n) {
+            nthFibo = first + second;
+            first = second;
+            second = nthFibo;
+            counter++;
+        }
+        }
+        System.out.println(nthFibo);
+        }
+    }
+}
