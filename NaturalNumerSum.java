bdfcbc3172155331ba12b7089c126ac6ff4c4054
Sachin Choudhary <sachin.choudhary58@gmail.com>
2023-03-24 17:47:24 +0530

    Create NaturalNumerSum.java

---
+++ NaturalNumerSum.java
@@ -0,0 +1,13 @@
+class NaturalNumberSum {
+    public static void main(String[] args) {
+        int sum = 0;
+        int counter = 1;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while(counter<=n) {
+            sum = sum+counter;
+            counter += 1;
+        }
+        System.out.println(sum);
+    }
+}
