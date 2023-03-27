ddd9f4448173868c95b0ba61fedd04d151dcb991
Sachin Choudhary <sachin.choudhary58@gmail.com>
2023-03-27 22:02:57 +0530

    Create LCM.java

---
+++ LCM.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class LCM {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int i = 2;
+        int lcm = 1;
+        while(n > 1 || m > 1) {
+            while(n%i == 0 || m%i == 0) {
+                if(n%i == 0) {
+                    n = n/i;
+                }
+                if(m%i == 0) {
+                    m = m/i;
+                }
+                lcm = lcm * i;
+            }
+            i++;
+        }
+        System.out.println(lcm);
+    }
+}
